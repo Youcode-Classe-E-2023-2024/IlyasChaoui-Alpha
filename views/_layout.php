2eee61e7720f667561d5f3cffd96eac7245f23dc
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-01-04 14:26:26 +0100

    Update _layout.php

--- views/_layout.php
+++ views/_layout.php
@@ -5,17 +5,33 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.x/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.0/dist/alpine.min.js" defer></script>
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/png" href="./assets/pictures/avito.png" />
     <title><?= ucfirst($page) ?></title>
-    <link rel="stylesheet" href="<?= PATH ?>assets/css/style.css">
+
+
+
+
+
 </head>
+<style>
+    #contact-form, #overlay {
+        display: none;
+    }
+    /* Add your additional CSS styling here */
+</style>
 <body>
-    <h1><?= ucfirst($page) ?> View</h1>
 
-    <main>
-        <?php include_once 'views/' . $page . '_view.php'; ?>
-    </main>
 
-    <footer></footer>
-    <script src="<?= PATH ?>assets/js/main.js"></script>
+<main>
+    <?php include_once 'views/' . $page . '_view.php'; ?>
+</main>
+
+<footer></footer>
+
+
 </body>
 </html>
